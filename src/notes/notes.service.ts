12c6b62cbf5201b503b503d3597e6acbf4110fbe
Nikita <dimbildon2281337@gmail.com>
2024-09-30 18:19:40 +0300

    fix

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -7,11 +7,13 @@ import { Note } from '../schemas/note.schema';
 import { User } from "../schemas/user.schema";
 import CreateNoteDto from "./dto/create-note.dto";
 import SearchNoteDto from "./dto/search-note.dto";
+import { Tag } from '../schemas/tag.schema';
 
 @Injectable()
 export class NotesService {
   constructor(@InjectModel(Note.name) private noteModel: Model<Note>,
-              @InjectModel(User.name) private userModel: Model<User>) {}
+              @InjectModel(User.name) private userModel: Model<User>,
+              @InjectModel(Tag.name) private tagModel: Model<Tag>) {}
 
   async findAll(userId: string, query: SearchNoteDto): Promise<Note[]> {
     const {searchQuery,
@@ -21,7 +23,6 @@ export class NotesService {
       filter} = query;
 
     const filters = {
-      owner: userId,
       $or: [
         { title: { $regex: searchQuery, $options: 'i' } },
         { description: { $regex: searchQuery, $options: 'i' } }
@@ -29,17 +30,19 @@ export class NotesService {
     };
 
     if (filter) {
-      Object.assign(filters, {tags : { $in: {name: [filter]}}});
+      const tag = await this.tagModel.findOne({ name: filter }).exec();
+      if (tag) {
+        Object.assign(filters, { tags: tag._id });
+      }
     }
 
     const skip = (page - 1) * limit;
-    return this.noteModel.find(filters)
+    return this.noteModel.find({ tags: filter })
       .sort({title: sortOrder === 'asc' ? 1 : -1})
       .skip(skip)
       .limit(limit)
       .exec();
   }
-
 
   async find(id: string): Promise<Note> {
     return this.noteModel.findById(id).exec();
